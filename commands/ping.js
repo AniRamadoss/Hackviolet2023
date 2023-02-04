cfb7de4175884d9e1090a654ec81efd078632169
AniRamadoss <aniruthan2002@gmail.com>
2023-02-04 15:02:28 -0500

    speech to text on discord works

--- commands/ping.js
+++ commands/ping.js
@@ -1,10 +1,44 @@
-const { SlashCommandBuilder } = require("discord.js");
+const {
+	SlashCommandBuilder,
+	ChatInputCommandInteraction,
+} = require("discord.js");
+const { joinVoiceChannel } = require("@discordjs/voice");
+const { witaiToken } = require("../config.json");
+const Transcriber = require("../transcriber");
+
+const transcriber = new Transcriber(witaiToken);
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName("ping")
 		.setDescription("Replies with Pong!"),
 	async execute(interaction) {
-		await interaction.reply("Pong!");
+		let channel = interaction.member.guild.channels.cache.get(
+			interaction.member.voice.channel.id
+		);
+		const connection = joinVoiceChannel({
+			channelId: channel.id,
+			guildId: channel.guild.id,
+			adapterCreator: channel.guild.voiceAdapterCreator,
+			selfDeaf: false,
+			selfMute: false,
+		});
+		connection.receiver.speaking.on("start", (userId) => {
+			transcriber
+				.listen(
+					connection.receiver,
+					userId,
+					interaction.client.users.cache.get(userId)
+				)
+				.then((data) => {
+					if (!data.transcript.text) return;
+					let text = data.transcript.text;
+					let user = data.user;
+
+					console.log("user: " + user + " said: " + text);
+				});
+		});
+
+		await interaction.reply("pong!");
 	},
 };
